6b420f8cfaa4c9193b7c1fdbff6f050939e89e4a
Thobias Stahlschmidt <thobias_stahlschmidt@whirlpool.com>
2023-08-29 19:10:05 -0300

    feat: add xml layout

--- app/src/main/java/com/stahlt/fuel_register_app/MainActivity.kt
+++ app/src/main/java/com/stahlt/fuel_register_app/MainActivity.kt
@@ -2,10 +2,14 @@ package com.stahlt.fuel_register_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun btAddOnClick(view: View) {}
+    fun btSearchOnClick(view: View) {}
 }

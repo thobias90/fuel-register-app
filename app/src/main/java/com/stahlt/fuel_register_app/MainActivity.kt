ef6d922b2529fc9adb76c8f773c2e7b69536c495
Thobias Stahlschmidt <thobias_stahlschmidt@whirlpool.com>
2023-08-29 19:54:01 -0300

    feat: add variables initialization

--- app/src/main/java/com/stahlt/fuel_register_app/MainActivity.kt
+++ app/src/main/java/com/stahlt/fuel_register_app/MainActivity.kt
@@ -1,13 +1,30 @@
 package com.stahlt.fuel_register_app
 
+import android.database.sqlite.SQLiteDatabase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var etFuelCode: EditText
+    private lateinit var etCity: EditText
+    private lateinit var etFuelAmount: EditText
+    private lateinit var dataBase: SQLiteDatabase
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        etFuelCode = findViewById(R.id.etFuelCode)
+        etCity = findViewById(R.id.etCity)
+        etFuelAmount = findViewById(R.id.etFuelAmount)
+
+        dataBase = SQLiteDatabase.openOrCreateDatabase(this.getDatabasePath("dbfile.sqlite"),
+            null)
+        dataBase.execSQL("CREATE TABLE IF NOT EXISTS refuel " +
+                "(_id INTEGER PRIMARY KEY AUTOINCREMENT, fuel TEXT, city TEXT, amount TEXT)")
     }
 
     fun btAddOnClick(view: View) {}

5be3d0e4ed8ae8e98e063e26d4cbb70642bcd51c
Thobias Stahlschmidt <thobias_stahlschmidt@whirlpool.com>
2023-08-29 18:46:06 -0300

    first commit

---
+++ app/src/main/java/com/stahlt/fuel_register_app/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.stahlt.fuel_register_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
